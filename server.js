fce40a51050857756a058ec9b9f483c40123618b
Ysquid <ahk648@gmail.com>
2022-11-22 09:18:00 -0600

    quotes refactored wtih ID. POST updated to add ID. Comments

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
+//GET all quotes if no person, or get quotes matching person
 app.get("/api/quotes", (req, res, next) => {
   //for some reason if(req.query == {}) does not work
   //use Object.keys instead to ensure length is 0
   if (Object.keys(req.query).length === 0) {
     const response = { quotes: quotes };
     res.send(response);
+    console.log(quotes)
   } else {
     const person = req.query.person;
     const filteredQutoes = quotes.filter((quote) => quote.person === person);
@@ -21,14 +23,17 @@ app.get("/api/quotes", (req, res, next) => {
   }
 });
 
+//GET random quote
 app.get("/api/quotes/random", (req, res) => {
   const randomQuote = { quote: getRandomElement(quotes) };
   console.log(randomQuote);
   res.send(randomQuote);
 });
 
+//POST new quote
 app.post("/api/quotes", (req, res) => {
   const newQuote = {
+    id: quotes.length,
     quote: req.query.quote,
     person: req.query.person
   };
@@ -36,6 +41,7 @@ app.post("/api/quotes", (req, res) => {
   if (newQuote.quote && newQuote.person) {
     quotes.push(newQuote);
     res.send({ quote: newQuote });
+    res.send(quotes)
   } else {
     res.status(400).send();
   }
